c50e2eae2ade2c82aac48845f015eeb0dbe22a2b
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-12-06 21:12:01 +0100

    Initial Commit

---
+++ SL-Sweet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SL-Sweet
+//
+//  Created by Jia-Jiuan Tsai on 2021-12-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

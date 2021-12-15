3501a410111053a68947c9a9ac2e547817d1a107
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-12-15 12:54:34 +0100

    Add loadingSpinner

--- SL-Sweet/ViewController.swift
+++ SL-Sweet/ViewController.swift
@@ -10,24 +10,37 @@ import UIKit
 class ViewController: UIViewController {
     
     let fetcher = TrafficStatusFetcher()
+    @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        fetcher.fetchData() { result in
+        loadData()
+    }
+    
+    func loadData() {
+        showLoadingSpinner()
+        fetcher.fetchData() { [weak self] result in
+            guard let self = self else { return }
+            self.hideLoadingSpinner()
             switch result {
             case .success(let data):
                 print(data.ResponseData.TrafficTypes[0].Events)
             case .failure(let error):
                 print(error)
-            
             }
-            
         }
     }
     
+    func showLoadingSpinner() {
+        loadingSpinner.isHidden = false
+        loadingSpinner.startAnimating()
+    }
     
+    func hideLoadingSpinner() {
+        loadingSpinner.stopAnimating()
+        loadingSpinner.isHidden = true
+    }
     
     
     

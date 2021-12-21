7d90969113a3be256515b1b4bccc6ad1cd00213d
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-12-21 22:01:16 +0100

    Add back showCamera (gallery) function back in RecognitionVC

--- SL-Sweet/ViewControllers/TextRecognitionVC.swift
+++ SL-Sweet/ViewControllers/TextRecognitionVC.swift
@@ -6,28 +6,16 @@
 //
 
 import UIKit
-import Vision
 
-class TextRecognitionVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+class TextRecognitionVC: UIViewController {
     
     @IBOutlet weak var loadingSpinner: UIActivityIndicatorView!
     
     private let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "recognitionDisplayVC")
     
     
-    private let label: UILabel = {
-       let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        return label
-    }()
     
-    private let imageView: UIImageView = {
-       let imageView = UIImageView()
-        //imageView.image = UIImage(named: "boardingGateImage")
-        imageView.contentMode = .scaleAspectFit
-        return imageView
-    }()
     
     
 
@@ -35,8 +23,7 @@ class TextRecognitionVC: UIViewController, UIImagePickerControllerDelegate, UINa
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
-        view.addSubview(label)
-        view.addSubview(imageView)
+        
         
         loadingSpinner.isHidden = true
         
@@ -45,18 +32,7 @@ class TextRecognitionVC: UIViewController, UIImagePickerControllerDelegate, UINa
         //recognizeText(image: imageView.image)
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        imageView.frame = CGRect(x: 20,
-                                 y: view.frame.size.height / 4 + 40,
-                                 width: view.frame.size.width - 40,
-                                 height: view.frame.size.width - 80)
-        
-        label.frame = CGRect(x: 20,
-                             y: view.frame.size.height / 2 + 100,
-                             width: view.frame.size.width - 40,
-                             height: 200)
-    }
+    
     
     
     @IBAction func showGallery(_ sender: UIButton) {
@@ -71,85 +47,26 @@ class TextRecognitionVC: UIViewController, UIImagePickerControllerDelegate, UINa
     }
     
     
-    // MARK: - Camera
     
-    private func showCamera() {
-        let picker = UIImagePickerController()
-        picker.sourceType = .photoLibrary
-        picker.delegate = self
-        present(picker, animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        picker.dismiss(animated: true, completion: nil)
-        
-        guard let photo = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-        
-        imageView.image = photo
-        imageView.isHidden = false
-        recognizeText(image: imageView.image)
-        
-    }
     
     // MARK: - Loading Spinner
     
     private func showSpinner() {
         loadingSpinner.isHidden = false
         loadingSpinner.startAnimating()
-        label.isHidden = true
-        imageView.isHidden = true
+        //label.isHidden = true
+        //imageView.isHidden = true
     }
     
     private func hideLoadingSpinner() {
         loadingSpinner.isHidden = true
         loadingSpinner.stopAnimating()
-        label.isHidden = false
-        imageView.isHidden = false
+        //label.isHidden = false
+        //imageView.isHidden = false
     }
     
     
-    // MARK: - Text Recognition
-    
-    private func recognizeText(image: UIImage?) {
-        
-        DispatchQueue.global().async {
-            guard let cgImage = image?.cgImage else { return }
-            
-            // Handler
-            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
-            
-            // Request
-            let request = VNRecognizeTextRequest { [weak self] request, error in
-                guard let observations = request.results as? [VNRecognizedTextObservation], error == nil else {
-                    return }
-                let text = observations.compactMap({
-                    $0.topCandidates(1).first?.string
-                }).joined(separator: ", ")
-                
-                DispatchQueue.main.async {
-                    self?.label.text = text
-                    self?.hideLoadingSpinner()
-                }
-                
-                print(text)
-            }
-            // Process request
-            do {
-                try handler.perform([request])
-            }
-            catch {
-                print(error)
-            }
-        }
-        
-        
-        
-    }
+   
     
 
     /*

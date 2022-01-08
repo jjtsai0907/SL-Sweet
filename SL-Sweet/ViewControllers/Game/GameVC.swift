b1dd5e5fe4fefeb26c82ca8d5497feefdcda506f
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2022-01-08 22:26:40 +0100

    Create GameVC

---
+++ SL-Sweet/ViewControllers/Game/GameVC.swift
@@ -0,0 +1,18 @@
+//
+//  GameVC.swift
+//  SL-Sweet
+//
+//  Created by Jia-Jiuan Tsai on 2022-01-08.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}

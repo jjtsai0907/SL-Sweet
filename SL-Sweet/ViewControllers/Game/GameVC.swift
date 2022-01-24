f74dcccda24ee52bd721d333a2f3722442246cd6
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2022-01-24 09:45:34 +0100

    Ensure there is only one timer in GameVM & able to reset game

--- SL-Sweet/ViewControllers/Game/GameVC.swift
+++ SL-Sweet/ViewControllers/Game/GameVC.swift
@@ -55,14 +55,14 @@ class GameVC: UIViewController {
             DispatchQueue.main.async { [self] in
                 guard let self = self else { return }
                 
-                self.progressBar.setProgress(value / 10.0 , animated: true)
+                self.progressBar.setProgress(value / 3.0 , animated: true)
                 if value < 0 {
                     //self?.runButton.isEnabled = false
                     
                     let alert = UIAlertController(title: "Great Job!", message: "Because of your hard work, \(self.gameVM.amountOfPassenger) lazy pigs can finally make it to work just on time", preferredStyle: .alert)
                     alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-                        //self.gameVM.resetGame()
-                        //self.progressBar.setProgress(1.0, animated: true)
+                        self.gameVM.resetGame()
+                        self.amountOfPassenger.text = " \(String(self.gameVM.amountOfPassenger)) Passengers"
                         print("okay")
                     }))
                     self.present(alert, animated: true, completion: nil)
